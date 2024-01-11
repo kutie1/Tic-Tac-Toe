7c6f8300afe1fc4ceec115110b0dae4c8240ac1a
chabaniuk-m <chabanyuk.maks@gmail.com>
2024-01-11 13:38:17 +0200

    convert board into string

--- src/game/Board.java
+++ src/game/Board.java
@@ -53,6 +53,14 @@ public class Board {
 
     @Override
     public String toString() {
-        return super.toString();
+        // TODO: display board
+        return String.format("""
+                | %s | %s | %s |
+                | %s | %s | %s |
+                | %s | %s | %s |""",
+                matrix[0][0], matrix[0][1], matrix[0][2],
+                matrix[1][0], matrix[1][1], matrix[1][2],
+                matrix[2][0], matrix[2][1], matrix[2][2]
+                );
     }
 }
